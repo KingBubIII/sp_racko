c1b7172f346adf0d298e26aff6ea46bea23e04c2
Caleb Richardson <richardsoncaleb60@gmail.com>
2023-06-01 22:42:19 -0600

    created main file and setup function

---
+++ mmain.py
@@ -0,0 +1,15 @@
+from ds_classes import *
+import random
+
+def setup(size):
+    deck = LinkedList(1)
+    for i in range(2, size+1):
+        pos = random.randint(0,deck.length)
+        
+        if pos == 0:
+            deck.prepend(i)
+        elif pos == deck.length:
+            deck.append(i)
+        elif pos < deck.length:
+            deck.insert(pos, i)
+    deck.printList()
