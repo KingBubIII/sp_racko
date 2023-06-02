b759e3f036a6b45e8e003ba2600ae45da2f294e6
Caleb Richardson <richardsoncaleb60@gmail.com>
2023-06-01 22:23:59 -0600

    refactored LL class to work with Node class

--- ds_classes.py
+++ ds_classes.py
@@ -4,8 +4,8 @@ class Node:
         self.next = None
 
 class LinkedList:
-    def __init__(self, value) -> None:
-        self.head = {"value" : value, "next" : None}
+    def __init__(self, init_value) -> None:
+        self.head = Node(init_value)
         self.tail = self.head
         self.length = 1
 
@@ -13,8 +13,8 @@ class LinkedList:
         currNode = self.head
         myArr = []
         while not currNode is None:
-            myArr.append(currNode["value"])
-            currNode = currNode["next"]
+            myArr.append(currNode.value)
+            currNode = currNode.next
         print(myArr)
 
     def Traverse(self, target):
@@ -22,45 +22,48 @@ class LinkedList:
         currNode = self.head
 
         while not index == target:
-            currNode = currNode["next"]
+            currNode = currNode.next
             index += 1
 
         return currNode
     
-    def append(self, value):
-        newNode = {"value" : value, "next" : None}
-        self.tail["next"] = newNode
+    def append(self, new_value):
+        newNode = Node(new_value)
+        self.tail.next = newNode
         self.tail = newNode
         self.length += 1
     
-    def prepend(self, value):
-        newNode = {"value" : value, "next" : None}
-        newNode["next"] = self.head
+    def prepend(self, new_value):
+        newNode = Node(new_value)
+        newNode.next = self.head
         self.head = newNode
         self.length += 1
 
-    def insert(self, targetI, value):        
-        newNode = {"value" : value, "next" : None}
+    def insert(self, targetI, new_value):        
+        newNode = Node(new_value)
         pre = self.Traverse(targetI-1)
 
-        newNode["next"] = pre["next"]
-        pre["next"] = newNode
+        newNode.next = pre.next
+        pre.next = newNode
         
         self.length += 1
         return
 
     def remove(self, index):
         pre = self.Traverse(index-1)
-        pre["next"] = pre["next"]["next"]
+        if pre.next.next == None:
+            self.tail = pre
+        pre.next = pre.next.next
+
         self.length -= 1
         return
     
     def reverse(self):
         currNode = self.head
         prevNode = None
-        while self.tail["next"] == None:
-            tempNode = currNode["next"]
-            currNode["next"] = prevNode
+        while self.tail.next == None:
+            tempNode = currNode.next
+            currNode.next = prevNode
 
             prevNode = currNode
             currNode = tempNode
